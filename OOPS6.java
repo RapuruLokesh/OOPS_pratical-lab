28ced0640ee6a9199e3f635ba0ca860cd9bd5062
RapuruLokesh <2420030534@klh.edu.in>
2025-07-26 11:37:32 +0530

    Add files via upload

---
+++ OOPS6.java
@@ -0,0 +1,23 @@
+package LOKESH;
+
+public class OOPS6{
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+        char a ='G';
+        int i = 10;
+        byte b =4;
+        short s = 56;
+        double d = 4.3555555;
+        float f = 4.74735f;
+        long l = 1212121;
+         System.out.println("char:" +a);
+         System.out.println("int:" +i);
+         System.out.println("byte:" +b);
+         System.out.println("short:" +s);
+         System.out.println("double:" +d);
+         System.out.println("float:" +f);
+         System.out.println("long:" +l);
+ }
+
+}
