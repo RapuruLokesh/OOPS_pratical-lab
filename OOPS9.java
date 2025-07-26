2d787c67f548767f477169ce66f5fac7186d4682
RapuruLokesh <2420030534@klh.edu.in>
2025-07-26 11:38:06 +0530

    Add files via upload

---
+++ OOPS9.java
@@ -0,0 +1,20 @@
+package LOKESH;
+
+
+import java.io.*;
+
+public class OOPS9 {
+ public static void main(String[] args) {
+     int i = 10;
+
+    
+     long l = i;
+
+     
+     double d = i;
+
+     System.out.println("Integer: " + i);
+     System.out.println("Long: " + l);
+     System.out.println("Double: " + d);
+ }
+}
